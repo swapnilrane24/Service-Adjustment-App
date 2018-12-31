568b333965017f1ca9943776720bf1bf7e11bbd8
Swapnil Rane <swapnilrane24@gmail.com>
2018-12-31 22:47:27 +0530

    Added Amazon S3
    Imported Amazon Storage Service to the project.

---
+++ Assets/Insurance App/assets/Scripts/AWSManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+
+public class AWSManager : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+    }
+}
